d090af746d452e71e66d71fdbe789eb3f6e88d1c
Harsh Parekh <harsh_parekh@outlook.com>
2022-04-14 11:40:34 -0400

    Add module containing feature extractors.

---
+++ hades/features.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def fn_line_length(x: np.ndarray):
+    return np.sum(np.absolute(np.diff(x, axis=0)), axis=0)
+
+
+def fn_signed_area(x: np.ndarray):
+    return np.sum(x, axis=0)
+
+
+def fn_area(x: np.ndarray):
+    return np.sum(np.absolute(x), axis=0)
+
+
+def fn_energy(x: np.ndarray):
+    return np.linalg.norm(x, axis=0) ** 2
+
+
+def fn_zero_crossings(x: np.ndarray):
+    x_cent = x - np.mean(x, axis=0)
+    x_shift = np.hstack([x_cent[:, 1:], x_cent[:, -1].reshape((-1, 1))])
+    x_cross = (x_cent * x_shift) < 0
+    return np.sum(x_cross, axis=0)
